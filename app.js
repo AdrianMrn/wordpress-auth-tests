56b5ef58141f377529535a8b7cf9bdb6b8d21a8e
Adriaan Marain <adriaanmarain300@gmail.com>
2018-03-27 14:55:39 +0200

    adding custom fields + relation

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 require('dotenv').config();
 const axios = require('axios');
+const moment = require('moment');
 
 const apiUri = process.env.API_URI;
+
+let config;
+const post = {
+    // WP reserved fields (https://developer.wordpress.org/rest-api/reference/posts/#create-a-post)
+    content: 'leegggogoooooo',
+    title: 'very talk much speak',
+    excerpt: 'wow',
+    status: 'publish',
+    // CPT
+    type: 'talks',
+    // ACFs
+    fields: {
+        type: 'talk',
+        start_datetime: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
+        description: 'Very very very nice talk, incredible!',
+        language: 'English',
+        speaker: { ID: 207 }
+    }
+};
 
 getToken = (credentials, callback) => {
     console.log(`Starting test for user ${credentials.username}`)
@@ -14,19 +34,15 @@ getToken = (credentials, callback) => {
         });
 }
 
-testPost = (token) => {
+testPost = (token, callback) => {
     console.log(`Running POST`);
 
-    const postData = {
-        content: 'get kekbaited',
-        title: '5 ways top kek is kek',
-        excerpt: 'kek'
-    };
-    const config = { headers: { 'Authorization': "Bearer " + token } };
+    config = { headers: { 'Authorization': "Bearer " + token } };
 
-    axios.post(`${apiUri}/wp/v2/posts`, postData, config)
+    axios.post(`${apiUri}/wp/v2/${post.type}`, post, config)
         .then(function (response) {
             console.log(response.data);
+            callback(response.data.id);
         })
         .catch(function (err) {
             console.log(err);
@@ -36,12 +52,17 @@ testPost = (token) => {
 const token = getToken({ username: process.env.USER_NAME, password: process.env.USER_PASSWORD }, (response, err) => {
     if (err) {
         console.log("We ran into an error:");
-        console.log(err);
-        /* err.response.data should provide more info about the error,
-        unless we didn't get a response at all (eg server down) */
+        console.log(err); //err.response.data should provide more info about the error, unless we didn't get a response at all (eg server down)
     } else {
         console.log(`Got token: ${response.token}`);
-        testPost(response.token);
+        testPost(response.token, (postId) => {
+            axios.put(`${apiUri}/acf/v3/${post.type}/${postId}`, post, config)
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (err) {
+                    console.log(err.response.data);
+                });
+        });
     }
 });
-
